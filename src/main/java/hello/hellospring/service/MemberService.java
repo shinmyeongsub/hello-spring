47db86fa65430d0bfeb44a2229733ace02d7aee5
shinmyeongsub <joyful2529@naver.com>
2021-11-29 19:01:38 +0900

    2021/11/29

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -5,10 +5,12 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -20,6 +22,7 @@ public class MemberService {
     /**
      * 회원가입
      */
+
     public Long join(Member member){
         // 같은 이름 중복 검증
         validateDuplicateMember(member);
